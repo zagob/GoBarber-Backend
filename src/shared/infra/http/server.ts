f88da964a02fdaab420697aac151a54bb96c349c
zagob <matheusus1_23@hotmail.com>
2021-01-14 10:48:13 -0300

    configura porta

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -39,6 +39,8 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
     })
 });
 
-app.listen(3333, () => {
-    console.log('🚀  Server started on port 3333!')
-});
+app.listen(process.env.PORT || 3333);
+
+// app.listen(3333, () => {
+//     console.log('🚀  Server started on port 3333!')
+// });
